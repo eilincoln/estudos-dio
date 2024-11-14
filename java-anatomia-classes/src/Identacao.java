c7762cdcc2d241c7c8fb12e12e24866fb9d913fa
Lincoln Berto <lincoln.santospro@gmail.com>
2024-11-14 10:18:44 -0300

    Adicionando classes novas para separar o aprendizado e aprendendo sobre identação no código.

---
+++ java-anatomia-classes/src/Identacao.java
@@ -0,0 +1,5 @@
+public class Identacao {
+    public static void main(String[] args) {
+        
+    }
+}
--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,21 @@
 public class MinhaClasse {
     public static void main (String [] args) {
         System.out.println("Hello World");
+
+        String nomeCarro = "Mercedes";
+
+        int anoFabricacao = 2022;
+
+        System.out.printf("O marca do carro é %s e seu ano de fabricação é %d \n", nomeCarro, anoFabricacao);
+
+        //Usando o outro método que coloquei
+        String primeiroNome = "Lincoln";
+        String segundoNome = "Berto";
+        System.out.println(nomeCompleto(primeiroNome, segundoNome));
+    }
+
+    // Outro método para testar
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
