3ae017f1429647230115eae3cb7d616e4c780170
Lincoln Berto <lincoln.santospro@gmail.com>
2024-11-18 11:06:57 -0300

    Adicionando ultimo exercicio do curso e adicionando um novo projeto sobre métodos

---
+++ estruturas-de-repeticao-e-arrays/src/TesteJava.java
@@ -0,0 +1,6 @@
+public class TesteJava {
+    public static void main(String[] args) {
+
+        int num = 5, count = 1; while(count <= 3) { ++count; num += count; } System.out.println(num);
+    }
+}
