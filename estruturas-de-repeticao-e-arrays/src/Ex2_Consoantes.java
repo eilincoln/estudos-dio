1acc5cfa05c1acb2d3be529f59e1a7dfe15d147a
Lincoln Berto <lincoln.santospro@gmail.com>
2024-11-18 10:30:49 -0300

    Adicionando exercicio sobre vetores.

---
+++ estruturas-de-repeticao-e-arrays/src/Ex2_Consoantes.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ex2_Consoantes {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        @SuppressWarnings("unused")
+        int quantidadeConsoantes = 0;
+
+        int count = 0;
+        do {
+            System.out.print("Letra: ");
+            String letra = entrada.next();
+
+            // Aqui estou usando o "!" para negar, falando que se for alguma dessas letras eu não quero. Então vai dar false.
+            if(!(letra.equalsIgnoreCase("a") || letra.equalsIgnoreCase("e") ||
+                letra.equalsIgnoreCase("i") || letra.equalsIgnoreCase("o") ||
+                letra.equalsIgnoreCase("u"))){
+                    consoantes[count] = letra;
+                    quantidadeConsoantes++;
+            }
+
+            count++;
+        } while(count < consoantes.length);
+
+        for (String elemento : consoantes) {
+            if(elemento != null){
+                System.out.printf("%s ", elemento);
+            }
+        }
+
+        entrada.close();
+    }
+}
