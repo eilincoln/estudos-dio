ecd18a60a273490d301c27903d39716401682852
Lincoln Berto <lincoln.santospro@gmail.com>
2024-11-22 10:32:56 -0300

    Adicionando novos materiais sobre POO

---
+++ POO/src/Carro.java
@@ -0,0 +1,22 @@
+public class Carro {
+    String cor;
+    String modelo;
+    int capacidadeDoTanque;
+
+    //Construtor
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+}
