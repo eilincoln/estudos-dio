11640f220d6b868fdaba36c53440a227de82f753
Lincoln Berto <lincoln.santospro@gmail.com>
2024-12-09 10:37:36 -0300

    mais sobre objetos

--- workspace-eclipe/primeiro-projeto/src/POO/Aluno.java
+++ workspace-eclipe/primeiro-projeto/src/POO/Aluno.java
@@ -1,5 +1,33 @@
 package POO;
 
+//Minha primeira classe/objeto que representa um aluno.
 public class Aluno {
 
+		/*
+		 * Esses são os atributos da classe.
+		 * */
+		String nome;
+		int idade;
+		String dataNascimneto;
+		String registroGeral;
+		String numeroCpf;
+		String nomeMae;
+		String nomePai;
+		String dataMatricula;
+		String nomeEscola;
+		String serieMatriculado;
+		
+
+		public Aluno() {/*Isso é feito sozinho quando eu crio um aluno lá fora da classe*/
+			// TODO Auto-generated constructor stub
+		}
+		
+		public Aluno(String nomePadrao) {
+			nome = nomePadrao;
+		}
+		
+		public Aluno(String nomePadrao, int idadePadrao) {
+			nome = nomePadrao;
+			idade = idadePadrao;
+		}
 }
--- workspace-eclipe/primeiro-projeto/src/POO/ClasseParaObjeto.java
+++ workspace-eclipe/primeiro-projeto/src/POO/ClasseParaObjeto.java
@@ -1,5 +1,20 @@
 package POO;
 
 public class ClasseParaObjeto {
-
+	public static void main(String[] args) {
+		
+		
+		//Isso são construtores de objetos
+		//Aqui é uma instancia (uma criação de objeto)
+		Aluno aluno1 = new Aluno(); //Construtores padrões.
+		
+	  //Classe   referencia    instanciar para criação
+		Aluno    aluno2 =      new Aluno();
+		
+		Aluno aluno3 = new Aluno("Lincoln"); //Esse aluno já vem com um nome padrão
+		
+		Aluno aluno4 = new Aluno("Kamilly", 20); // Vem com um novo padrão igual criado na classe.
+		
+		
+	}
 }
