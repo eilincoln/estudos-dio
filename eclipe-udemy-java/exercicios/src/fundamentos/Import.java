4abc61e8cd5061491cbe84aeb2b443372c46fb6a
Lincoln Berto <lincoln.santospro@gmail.com>
2024-12-09 19:40:45 -0300

    trabalhando com imports

---
+++ eclipe-udemy-java/exercicios/src/fundamentos/Import.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+import java.util.Date; //usado para poder usar o tipo Date
+
+public class Import {
+	
+	public static void main(String[] args) {
+		
+		/*tem coisas que não tem em java a todo momneto, então tenho que importar!*/
+		Date dia = new Date();
+		System.out.println(dia);
+	}
+}
