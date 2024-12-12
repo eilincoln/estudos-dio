62c431469f708c42b32639e6bf3a06f45ddfcffb
Lincoln Berto <lincoln.santospro@gmail.com>
2024-12-12 19:27:49 -0300

    Adicionando mais sobre construtores em JAVA

--- eclipe-udemy-java/exercicios/src/classe/ProdutoTeste.java
+++ eclipe-udemy-java/exercicios/src/classe/ProdutoTeste.java
@@ -4,16 +4,16 @@ public class ProdutoTeste { // nome da classe
 	
 	public static void main(String[] args) {
 		
-		Produto produto1 = new Produto(); // como criar um produto novo
-		produto1.nome = "iPhone 15";
-		produto1.preco = 1000.00;
-		produto1.desconto = 0.25;
+		Produto produto1 = new Produto("iPhone 15", 1000.00, 0.25); // como criar um produto novo
+//		produto1.nome = "iPhone 15";
+//		produto1.preco = 1000.00;
+//		produto1.desconto = 0.25;
 
 		
-		Produto produto2 = new Produto();
-		produto2.nome = "Notebook";
-		produto2.preco = 4356.98;
-		produto2.desconto = 0.50;
+		Produto produto2 = new Produto("Notebook", 4356.98, 0.50);
+//		produto2.nome = "Notebook";
+//		produto2.preco = 4356.98;
+//		produto2.desconto = 0.50;
 		
 		
 		System.out.println("O nome do produto 1 é: " + produto1.nome);
