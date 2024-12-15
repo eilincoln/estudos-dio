303371457f8d37ef71cd0fc2befa62c4235a1d18
Lincoln Berto <lincoln.santospro@gmail.com>
2024-12-15 13:56:14 -0300

    vendo um pouco de matriz

---
+++ eclipe-udemy-java/exercicios/src/arrays/Matriz.java
@@ -0,0 +1,32 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Matriz {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Quantos alunos: ");
+		int quantidadeAlunos = entrada.nextInt();
+		
+		System.out.print("Quantas notas por aluno: ");
+		int quantidadeNotas = entrada.nextInt();
+		
+		double notasDaTurma[][] = new double[quantidadeAlunos][quantidadeNotas];
+		
+		double total = 0;
+		for (int i = 0; i < notasDaTurma.length; i++) {
+			for (int j = 0; j < notasDaTurma[i].length; j++) {
+				System.out.printf("Informe a nota %d do aluno %d: ",j + 1, i + 1);
+				notasDaTurma[i][j] = entrada.nextDouble();
+				total += notasDaTurma[i][j];
+			}
+		}
+		
+		double media = total / (quantidadeAlunos * quantidadeNotas);
+		System.out.println("Média da turma é: " + media);
+		
+		entrada.close();
+	}
+}
