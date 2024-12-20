7a7823a943f6e6e33692af6865410be12ee7aebf
eilincoln <lincoln.santospro@gmail.com>
2024-12-20 18:32:59 -0300

    Adicionando material sobre tipos primitivos

---
+++ CursoJavaUdemy/src/fundamentos/Import.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+
+public class Import {
+	public static void main(String[] args) {
+		System.out.println("...");
+		
+		double a = 2.0;
+		System.out.println(a);
+		
+		
+	}
+}
