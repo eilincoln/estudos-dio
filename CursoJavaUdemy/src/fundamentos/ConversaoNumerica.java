67b03c4c4ac11ffb76d61a472e4a1832590e4c35
eilincoln <lincoln.santospro@gmail.com>
2024-12-20 18:58:32 -0300

    Adicionando material sobre numeros primitivos e objetos

---
+++ CursoJavaUdemy/src/fundamentos/ConversaoNumerica.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class ConversaoNumerica {
+	
+	public static void main(String[] args) {
+		//Exemplo 01 - Conversão explícita.
+		float f = (float)0.1;
+		System.out.println(f);
+		
+		//Exemplo 2 - Conversão explícita
+		int i1 = (int) 7.9;
+		System.out.println(i1);
+	}
+}
