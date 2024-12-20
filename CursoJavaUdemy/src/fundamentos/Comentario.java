a55bb1e5f8c81e61f90029324659588232314697
eilincoln <lincoln.santospro@gmail.com>
2024-12-20 13:12:04 -0300

    Adicionando novos materiais de estudos sobre JAVA

---
+++ CursoJavaUdemy/src/fundamentos/Comentario.java
@@ -0,0 +1,30 @@
+/**
+ * Classe responsável por mostrar o uso de 3 tipos de comentários que o Java suporta.
+ * @author Lincoln Santos [lincoln.santospro@gmail.com]
+ * @version 1.0
+ * @since 2.2
+ */
+
+package fundamentos;
+
+public class Comentario {
+	
+	/**
+	 * Esse método é a porta de entrada de um programa Java.
+	 * 
+	 * @param args
+	 * parâmetro passadfos por linha de comando.
+	 */
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Imprime no console a palavra 'Primeiro' e
+		 * em seguida cria uma nova linha
+		 */
+		System.out.println("Hello");
+		
+		//Cuidado com os comentários óbvios
+		System.out.println("World");
+	}
+}
