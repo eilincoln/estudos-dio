b62dea483adb0f3593466a2501029d4c7c3ba80a
eilincoln <lincoln.santospro@gmail.com>
2024-12-22 19:12:36 -0300

    Adicionando construtores dentro das classes

--- CursoJavaUdemy/EstudosJava02/src/classe/Data.java
+++ CursoJavaUdemy/EstudosJava02/src/classe/Data.java
@@ -5,6 +5,16 @@ public class Data {
 	int mes;
 	int ano;
 	
+	Data(){
+		
+	}
+	
+	Data(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
 	String formatar() {
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
--- CursoJavaUdemy/EstudosJava02/src/classe/DataTeste.java
+++ CursoJavaUdemy/EstudosJava02/src/classe/DataTeste.java
@@ -3,11 +3,17 @@ package classe;
 public class DataTeste {
 	
 	public static void main(String[] args) {
-		Data nascimento = new Data();
-		nascimento.dia = 19;
-		nascimento.mes = 11;
-		nascimento.ano = 1987;
+		Data nascimento = new Data(06, 07, 1998);
+//		nascimento.dia = 19;
+//		nascimento.mes = 11;
+//		nascimento.ano = 1987;
 		
-		System.out.printf(nascimento.formatar());
+		Data casamento = new Data();
+		casamento.dia = 13;
+		casamento.mes = 12;
+		casamento.ano = 2027;
+		
+		System.out.println("Data do nascimento: " + nascimento.formatar());
+		System.out.println("Data do casamento: " + casamento.formatar());
 	}
 }
